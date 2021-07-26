e347c8d64a11c03e3f576243eee7d9fcd678c2b3
kmj <mjmjmj9840@gmail.com>
2021-07-26 16:28:00 +0900

    Spring 심화반 1주차 숙제 1. @Service, @Repository 사용하기 2. 패키지 정리하기

---
+++ src/main/java/com/sparta/springcore/service/ProductService.java
@@ -0,0 +1,54 @@
+package com.sparta.springcore.service;
+
+import com.sparta.springcore.dto.ProductMypriceRequestDto;
+import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class ProductService {
+    // 멤버 변수 선언
+    private final ProductRepository productRepository;
+    private final int MIN_PRICE = 100;
+
+    // 생성자: ProductService() 가 생성될 때 호출됨
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        // 멤버 변수 생성
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public List<Product> getProducts() {
+        // 멤버 변수 사용
+        return productRepository.findAll();
+    }
+
+    @Transactional
+    public Product createProduct(ProductRequestDto requestDto) {
+        // 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto);
+        productRepository.save(product);
+        return product;
+    }
+
+    @Transactional
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+        );
+
+        int myPrice = requestDto.getMyprice();
+        if (myPrice < MIN_PRICE) {
+            throw new IllegalArgumentException("관심 가격은 " + MIN_PRICE + "원 이상으로 설정해주세요. ");
+        }
+
+        product.updateMyPrice(myPrice);
+        return product;
+    }
+}
